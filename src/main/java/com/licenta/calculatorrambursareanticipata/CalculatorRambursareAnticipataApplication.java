7f4965869001e325112152398650cff3945aa6e7
Andra Oniga <andra.oniga@gmail.com>
2022-11-06 15:48:07 +0200

    Initial commit

---
+++ src/main/java/com/licenta/calculatorrambursareanticipata/CalculatorRambursareAnticipataApplication.java
@@ -0,0 +1,13 @@
+package com.licenta.calculatorrambursareanticipata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorRambursareAnticipataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorRambursareAnticipataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/licenta/calculatorrambursareanticipata/CalculatorRambursareAnticipataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licenta.calculatorrambursareanticipata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorRambursareAnticipataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
